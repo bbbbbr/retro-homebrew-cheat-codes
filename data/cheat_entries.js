8b3b7dd04d983e20d2212c5680010e8e79049994
bbbbbr <bbbbbr@users.noreply.github.com>
2025-08-01 03:11:00 -0700

    Entries: Add a couple more for testing

--- data/cheat_entries.js
+++ data/cheat_entries.js
@@ -37,4 +37,56 @@ const jsonCheatEntries = [
     "dateAdded": "2025-07-31",
 },
 
+
+
+{
+    "gameTitle": "FROM BELOW NES",
+    "developer": "Matt Hughson",
+    "consoleTags": "NES",
+    "cheatTypeTags": "Level",
+    "cheatEffect": "Start on Level 10-19",
+    "inputSequence": "Holding A when pressing START will start the game at 10 levels higher than the level you chose. For example, if you select level 3 and press A + START you will start on level 13.",
+    "whenToInput": "Settings screen (when you select level, music, etc)",
+    "activationIndicator": "",
+    "dateAdded": "2025-07-31",
+},
+
+
+{
+    "gameTitle": "FROM BELOW NES",
+    "developer": "Matt Hughson",
+    "consoleTags": "NES",
+    "cheatTypeTags": "Level",
+    "cheatEffect": "Start on Level 29",
+    "inputSequence": "Select level 9, and then hold SELECT and press START.",
+    "whenToInput": "Settings screen (when you select level, music, etc)",
+    "activationIndicator": "",
+    "dateAdded": "2025-07-31",
+},
+
+{
+    "gameTitle": "FROM BELOW NES",
+    "developer": "Matt Hughson",
+    "consoleTags": "NES",
+    "cheatTypeTags": "Sound Test",
+    "cheatEffect": "Prints an image for any unlocked trophy from the Game Boy Printer",
+    "inputSequence": "Enter the Konami Code: UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A, START",
+    "whenToInput": "Title screen",
+    "activationIndicator": "You will hear a ding and be taken to the sound test immediately",
+    "dateAdded": "2025-07-31",
+},
+
+{
+    "gameTitle": "Sneak 'n Peek",
+    "developer": "KHAN Games",
+    "consoleTags": "NES",
+    "cheatTypeTags": "Graphics",
+    "cheatEffect": "Switches the graphics to the original Atari player sprites",
+    "inputSequence": "UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A, START",
+    "whenToInput": "Title screen",
+    "activationIndicator": "",
+    "dateAdded": "2025-07-09"
+}
+
+
 ];
